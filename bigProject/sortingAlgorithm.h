d932492bb4e51393dfa28e62a234f4c90d7ebd4d
penquilz22 <huydio2202@gmail.com>
2025-03-29 18:29:33 +0700

    Update sortingAlgorithm.h

--- bigProject/sortingAlgorithm.h
+++ bigProject/sortingAlgorithm.h
@@ -3,18 +3,20 @@
 
 #include <iostream>
 #include <cmath>
-
-void reverseArray(int arr[], int n);
-
-void heapify(int arr[], int n, int i);
-void heapSort(int arr[], int n);
-
-int partition(int arr[], int low, int high);
-void quickSort(int arr[], int low, int high, int depthLimit);
-
-void insertionSort(int arr[], int low, int high);
-
-void introSort(int arr[], int n);
+template <class T>
+void reverseArray(T arr[], int n);
+template <class T>
+void heapify(T arr[], int n, int i);
+template <class T>
+void heapSort(T arr[], int n);
+template <class T>
+int partition(T arr[], int low, int high);
+template <class T>
+void quickSort(T arr[], int low, int high, int depthLimit);
+template <class T>
+void insertionSort(T arr[], int low, int high);
+template <class T>
+void introSort(T arr[], int n);
 template <class T>
 void MergeSort(T a[],int n);
 #endif // !sortingAlgorithm_h
