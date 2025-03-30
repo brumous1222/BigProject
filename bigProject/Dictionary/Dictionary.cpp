6364368e0ecd8dca11bf72f476aa08b8a460fd96
Hung127 <hunghungtt05@gmail.com>
2025-03-30 21:51:15 +0700

    Formated code

--- bigProject/Dictionary/Dictionary.cpp
+++ bigProject/Dictionary/Dictionary.cpp
@@ -1,59 +1,45 @@
 #include "Dictionary.h"
 
-void deleteAllDigits(string &word)
-{
+void deleteAllDigits(string &word) {
     int len = word.length();
-    if (len <= 0)
-    {
+    if (len <= 0) {
         return;
     }
-    while (word[len - 1] >= '0' && word[len - 1] <= '9')
-    {
+    while (word[len - 1] >= '0' && word[len - 1] <= '9') {
         word[len - 1] = '\0';
         len--;
     }
 }
 
-vector<string> loadDict(const string &inFile) // get all words from dictionary, put them into a vector
-{
+vector<string> loadDict(const string &inFile) { // get all words from dictionary, put them into a vector
     ifstream input;
     input.open(inFile, ios::in);
     vector<string> dictionary;
     string line;
     int currIdx = 0;
-    if (!input.is_open()) // can not open the file
-    {
+    if (!input.is_open()) { // can not open the file
         cout << "Failed to open file" << endl;
         return dictionary;
     }
-    while (getline(input, line)) // try to read line by line
-    {
+    while (getline(input, line)) { // try to read line by line
         int delimIdx = line.find(DELIM); // find if in current line has DELIM
-        if (delimIdx != string::npos) // found, extract substring from first char to delimIdx - 1
-        {
+        if (delimIdx != string::npos) { // found, extract substring from first char to delimIdx - 1
             string word = line.substr(0, delimIdx); // extract the word;
-            if (word[delimIdx - 1] == '-' || word[0] == '-') // skip all suffixs and prefixs
-            {
+            if (word[delimIdx - 1] == '-' || word[0] == '-') { // skip all suffixs and prefixs
                 continue;
             }
             deleteAllDigits(word); // delete all digits from a word
             deleteAllSpaces(word);
             toLowerCase(word);
-            if (word.length() <= 0) // no words
-            {
+            if (word.length() <= 0) { // no words
                 continue;
-            }
-            else if (!dictionary.empty())
-            {
-                if (word == dictionary[dictionary.size() - 1]) // check if it is duplicated
-                {
+            } else if (!dictionary.empty()) {
+                if (word == dictionary[dictionary.size() - 1]) { // check if it is duplicated
                     continue;
                 }
-                if (word == "usage")
-                {
+                if (word == "usage") {
                     int type = line.find(SPECIAL_USAGE); // check if it is the real "Usage" word which on the line "Usage  n."
-                    if (type != delimIdx)
-                    {
+                    if (type != delimIdx) {
                         continue;
                     }
                 }
@@ -79,19 +65,16 @@ void toLowerCase(string &word) {
     }
 }
 
-void exportDict(const string &outFile, const vector<string> &dict) // export dict to file (each line is a word)
-{
+void exportDict(const string &outFile, const vector<string> &dict) { // export dict to file (each line is a word)
     ofstream output;
     output.open(outFile, ios::out);
-    if (!output.is_open()) // failed to open
-    {
+    if (!output.is_open()) { // failed to open
         cout << "Failed to open file" << endl;
         return;
     }
     int len = dict.size();
     output << len << endl; // added a line for len of dict array
-    for (int i = 0; i < len; i++)
-    {
+    for (int i = 0; i < len; i++) {
         output << dict[i] << endl;
     }
     output.close();
