ab19809aefe190ae63456ed847f9d94e0e4f1f78
Hung127 <hunghungtt05@gmail.com>
2025-03-30 21:40:46 +0700

    Fixed 'usage' special case in lowercase

--- bigProject/Dictionary/Dictionary.cpp
+++ bigProject/Dictionary/Dictionary.cpp
@@ -49,7 +49,7 @@ vector<string> loadDict(const string &inFile) // get all words from dictionary, 
                 {
                     continue;
                 }
-                if (word == "Usage")
+                if (word == "usage")
                 {
                     int type = line.find(SPECIAL_USAGE); // check if it is the real "Usage" word which on the line "Usage  n."
                     if (type != delimIdx)
